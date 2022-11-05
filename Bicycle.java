9c434b967e35b072393994b3f8834a36f98e051e
Jessmark-11 <116412092+Jessmark-11@users.noreply.github.com>
2022-11-05 18:55:15 +0800

    Update Bicycle.java

--- Bicycle.java
+++ Bicycle.java
@@ -1,1 +1,18 @@
 # OOP-Exercise-1
+class Bicycle{
+      //Data Member
+       private String ownerName',
+      //Constructor: Initializes the data member
+      public Bicycle(){
+               ownerName = "Unknown"',
+}
+      Returns the name of this bicycle's owner
+      public String getOwnerName(){
+              return ownerName',
+}
+      Assigns the name of this bucycle's owner
+      public void setOwnerName(Stringname){
+              ownerName = name:
+}
+
+
